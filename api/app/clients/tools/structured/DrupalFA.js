e294bee0d9a4fd8465ca83541a8a0dacac57d923
sepa85 <stefano@cleversoft.it>
2024-05-29 03:20:28 +0000

    Fix DrupalFA to work wth drupal well

--- api/app/clients/tools/structured/DrupalFA.js
+++ api/app/clients/tools/structured/DrupalFA.js
@@ -1,126 +1,192 @@
-/* eslint-disable no-useless-escape */
+const { OpenAI } = require("openai");
 const axios = require('axios');
+
 const { z } = require('zod');
 const { StructuredTool } = require('langchain/tools');
 const { logger } = require('~/config');
-const { forEach } = require('lodash');
 
 class DrupalFAAPI extends StructuredTool {
   constructor(fields) {
     super();
 
-    /* Used to initialize the Tool without necessary variables. */
     this.override = fields.override ?? false;
-    
     this.name = 'drupal_fa_api';
 
-    this.apiKey = this.getApiKey();
-    this.apiBaseURL = this.getBaseUrl();
+    this.apiKey = this.getEnvVariable('DRUPAL_FA_API_KEY');
+    this.apiBaseURL = this.getEnvVariable('DRUPAL_FA_API_BASE_URL');
+    this.openaiApiKey = this.getEnvVariable('OPENAI_API_KEY');
+    this.agentAssistantId = this.getEnvVariable('DRUPAL_AGENT_ASSISTANT_ID');
 
     this.description_for_model = `Give a prompt for another assistant to retrieve the query string. For example, "Find the latest articles about climate change."`;
-
     this.description = `A tool for querying the Drupal FA API. The input should be a prompt for the assistant to retrieve the query string. The output will be the raw text response from the API.`;
 
-    this.openaiApiKey = this.getOpenAiApiKey();
-    this.agentAssistantId = this.getAgentAssistantId();
-
-    this.description_for_model_OLD = `// Access and retrieve Drupal content using the JSON:API module.
-    // The JSON:API module provides a standardized way to interact with Drupal entities via RESTful web services.
-    // General guidelines:
-    // - Use GET requests to retrieve entities, collections, and related resources from Drupal.
-    // - Use node_type--node_type to find the bundle for a specific content type, if the bundle you tried is not right. The endpoint is /jsonapi/node_type/node_type.
-    // - Utilize filtering to limit the data returned in collections to only the entities you need (e.g., by content type, fields, etc.).
-    // - Use includes to embed related resource objects within a single response, reducing the need for multiple HTTP requests.
-    // - Apply pagination to handle large collections by splitting the response into multiple pages using page[offset] and page[limit] parameters.
-    // - Implement sorting to order the results based on specified fields using the sort parameter.
-    // - Handle errors gracefully, providing informative messages for debugging purposes.
-    // - Follow the JSON:API specification strictly to structure requests and responses properly.
-    // - Ensure the schema for each request aligns with the expected format for the corresponding Drupal entity.
-    // - Utilize proper JSON formatting for all requests and responses to maintain compliance with JSON:API standards.
-    // - Structure for GET requests:
-    //   - Fetching a collection of entities: /jsonapi/{entity_type}/{bundle}
-    //   - Fetching a specific entity by UUID: /jsonapi/{entity_type}/{bundle}/{uuid}
-    // - Examples:
-    //   - Fetching node types:
-    //     - GET /jsonapi/node_type/node_type - Retrieve a collection of node types.
-    //   - Fetching nodes:
-    //     - GET /jsonapi/node/articolo - Retrieve a collection of articolo nodes.
-    //     - GET /jsonapi/node/articolo/{uuid} - Retrieve a specific articolo node by UUID.
-    //   - Filtering:
-    //     - GET /jsonapi/node/articolo?filter[status]=1 - Retrieve only published articles.
-    //     - GET /jsonapi/node/articolo?filter[title][value]=Sample&filter[title][operator]=CONTAINS - Retrieve articles with "Sample" in the title.
-    //   - Includes:
-    //     - GET /jsonapi/node/articolo?include=field_author - Retrieve articles along with their author information.
-    //   - Pagination:
-    //     - GET /jsonapi/node/articolo?page[offset]=0&page[limit]=10 - Retrieve the first 10 articles.
-    //   - Sorting:
-    //     - GET /jsonapi/node/articolo?sort=-created - Retrieve articles sorted by creation date in descending order.`;
-
-    this.description_OLD = `Drupal JSON:API offers a standardized way to retrieve Drupal content via RESTful web services, supporting entity retrieval, filtering, including related resources, pagination and sorting.`;
-    
     this.schema = z.object({
       input: z.string().describe('Natural language query to Drupal following the guidelines'),
     });
 
+    this.openai = new OpenAI({
+      apiKey: this.openaiApiKey,
+    });
   }
 
   async fetchRawText(url) {
     try {
       const response = await axios.get(url, { responseType: 'text' });
-      return response.data;
+      return response;
     } catch (error) {
       logger.error('[DrupalFAAPI] Error fetching raw text:', error);
       throw error;
     }
   }
 
-  getApiKey() {
-    const apiKey = process.env.DRUPAL_FA_API_KEY || '';
-    if (!apiKey) {
-      throw new Error('Missing DRUPAL_FA_API_KEY environment variable.');
+  getEnvVariable(varName) {
+    const value = process.env[varName];
+    if (!value) {
+      throw new Error(`Missing ${varName} environment variable.`);
     }
-    return apiKey;
+    return value;
   }
 
-  getOpenAiApiKey() {
-    const openaiApiKey = process.env.OPENAI_API_KEY || '';
-    if (!openaiApiKey) {
-      throw new Error('Missing OPENAI_API_KEY environment variable.');
+  async createThread() {
+    try {
+      const thread = await this.openai.beta.threads.create();
+      return thread;
+    } catch (error) {
+      logger.error('[DrupalFAAPI] Error creating thread:', error);
+      throw error;
     }
-    return openaiApiKey;
   }
 
-  getAgentAssistantId() {
-    const assistantId = process.env.DRUPAL_AGENT_ASSISTANT_ID || '';
-    if (!assistantId) {
-      throw new Error('Missing DRUPAL_AGENT_ASSISTANT_ID environment variable.');
+  async addMessageToThread(threadId, content) {
+    try {
+      logger.info(`[DrupalFAAPI] Adding message to thread: ${content}`);
+      await this.openai.beta.threads.messages.create(threadId, {
+        role: 'user',
+        content: content,
+      });
+    } catch (error) {
+      logger.error(`[DrupalFAAPI] Error adding message to thread: ${content}`, error);
+      throw error;
     }
-    return assistantId;
   }
 
-  getBaseUrl() {
-    const baseUrl = process.env.DRUPAL_FA_API_BASE_URL || '';
-    if (!baseUrl) {
-      throw new Error('Missing DRUPAL_FA_API_BASE_URL environment variable.');
+  async runAssistant(threadId) {
+    try {
+      const response = await this.openai.beta.threads.runs.create(threadId, {
+        assistant_id: this.agentAssistantId,
+      });
+      return response.id;
+    } catch (error) {
+      logger.error('[DrupalFAAPI] Error running assistant:', error);
+      throw error;
     }
-    return baseUrl;
   }
+
+  async getAssistantResponse(threadId, runId) {
+    // TODO: Implement function call to check responde status by the agent
+    // https://mer.vin/2023/11/assistants-api-function-calling-in-node-js/
+    try {
+      logger.info(`[DrupalFAAPI] Assistant threadId: ${threadId}`);
+      logger.info(`[DrupalFAAPI] Assistant runId: ${runId}`);
+      
+      let runStatus = await this.openai.beta.threads.runs.retrieve(threadId, runId);
+
+      logger.info(`[DrupalFAAPI] Run status: ${runStatus}`);
+
+      let response = '';
+
+      if(runStatus.status === "completed"){
+        let messages = await this.openai.beta.threads.messages.list(threadId);
+        messages.data.forEach((msg) => {
+            const role = msg.role;
+            const content = msg.content[0].text.value;
+            if (role === 'assistant') {
+              response += content;
+            }
+        });
+        
+        // Set the interval to clear the interval
+        clearInterval(this.intervalId);
+
+
+      } else {
+        logger.info("Run is not completed yet.");
+      }
+
+
+      if (response === '') {
+        throw new Error('No response from assistant.');
+      } else {
+        logger.info(`[DrupalFAAPI] Assistant response: ${response}`);
+        return response;
+      }
+
+
+    } catch (error) {
+      logger.error('[DrupalFAAPI] Error retrieving assistant response:', error);
+      throw error;
+    }
+  }
+
+
 
   async createDrupalURL(prompt) {
     try {
-      const threadId = await this.createThread();
-      await this.addMessageToThread(threadId, prompt);
+      const thread = await this.createThread();
+      const threadId = thread.id;
+      logger.info(`[DrupalFAAPI] Created thread ${threadId}`);
+      await this.addMessageToThread(threadId, prompt.input);
       const runId = await this.runAssistant(threadId);
-      const endpoints = await this.getAssistantResponse(threadId, runId);
+      logger.info(`[DrupalFAAPI] Ran assistant ${runId}`);
 
-      // Test the endpoints and find the first with response 200
+      const getResponseWithInterval = () => {
+        return new Promise((resolve, reject) => {
+          const intervalId = setInterval(async () => {
+            try {
+              const response = await this.getAssistantResponse(threadId, runId);
+              if (response) {
+                clearInterval(intervalId);
+                resolve(response);
+              }
+            } catch (error) {
+              clearInterval(intervalId);
+              reject(error);
+            }
+          }, 10000);
+        });
+      };
+  
+      const response = await getResponseWithInterval();
+
+      // Convert json response to array
+      const endpoints = JSON.parse(response).endpoints;
+
+      logger.info(`[DrupalFAAPI] Endpoints: ${endpoints}`);
+      logger.info(`[DrupalFAAPI] Endpoints type: ${typeof endpoints}`);
+
       let endpoint = '';
 
-      for (const endpoint of endpoints) {
-        const url = `${this.apiBaseURL}${endpoint}`;
+      for (const ep of endpoints) {
+        // Check if it's a valid endpoint
+        if (!ep.startsWith('/jsonapi/')) {
+          logger.info(`[DrupalFAAPI] Skipping wrong endpoint ${ep}`);
+          continue;
+        }
+        let url = `${this.apiBaseURL}${ep}`;
+
+        // Append apikey to the query with api_key parameter
+        url = new URL(url);
+        url.searchParams.append('api_key', this.apiKey);
+        url = url.toString();
+
+        logger.info(`[DrupalFAAPI] Testing endpoint: ${url}`);
         try {
-          const response = await axios.get(url);
-          if (response.status === 200) {
+          const response = await this.fetchRawText(url);
+
+          // Extract json data to array
+          const responseData = JSON.parse(response.data);
+          
+          if (response.status === 200 && responseData.data.length > 0) {
             endpoint = url;
             break;
           }
@@ -130,7 +196,7 @@ class DrupalFAAPI extends StructuredTool {
       }
 
       if (!endpoint) {
-        throw new Error('No endpoint with response 200 found.');
+        throw new Error('No endpoint with response 200 and content found.');
       }
 
       return endpoint;
@@ -141,100 +207,17 @@ class DrupalFAAPI extends StructuredTool {
     }
   }
 
-  async createThread() {
-    const response = await axios.post('https://api.openai.com/v1/threads', {}, {
-      headers: {
-        'Authorization': `Bearer ${this.openaiApiKey}`,
-        'Content-Type': 'application/json',
-        'OpenAI-Beta': 'assistants=v2',
-      },
-    });
-    return response.data.id;
-  }
-
-  async addMessageToThread(threadId, content) {
-    await axios.post(`https://api.openai.com/v1/threads/${threadId}/messages`, {
-      role: 'user',
-      content: content,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${this.openaiApiKey}`,
-        'Content-Type': 'application/json',
-        'OpenAI-Beta': 'assistants=v2',
-      },
-    });
-  }
-
-  async runAssistant(threadId) {
-    const response = await axios.post(`https://api.openai.com/v1/threads/${threadId}/runs`, {
-      assistant_id: this.agentAssistantId,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${this.openaiApiKey}`,
-        'Content-Type': 'application/json',
-        'OpenAI-Beta': 'assistants=v2',
-      },
-    });
-    return response.data.id;
-  }
-
-  async getAssistantResponse(threadId, runId) {
-    const response = await axios.get(`https://api.openai.com/v1/threads/${threadId}/runs/${runId}/steps`, {
-      headers: {
-        'Authorization': `Bearer ${this.openaiApiKey}`,
-        'Content-Type': 'application/json',
-        'OpenAI-Beta': 'assistants=v2',
-      },
-    });
-    const messages = response.data.data;
-    for (const message of messages) {
-      if (message.type === 'message_creation' && message.role === 'assistant') {
-        return JSON.parse(message.content[0].text).endpoints;
-      }
-    }
-    throw new Error('No endpoints found in assistant response.');
-  }
-
-  // createDrupalJSONAPIURL(data) {
-  //   const { entity_type, bundle, uuid, filter, include, sort, page } = data;
-  //   let url = `${process.env.DRUPAL_FA_API_BASE_URL}/jsonapi/${entity_type}/${bundle}`;
-  //   if (uuid) {
-  //     url += `/${uuid}`;
-  //   }
-  //   const params = new URLSearchParams();
-  //   if (this.apiKey) {
-  //     params.append('api_key', this.apiKey);
-  //   }
-  //   if (filter) {
-  //     params.append('filter', filter);
-  //   }
-  //   if (include) {
-  //     params.append('include', include);
-  //   }
-  //   if (sort) {
-  //     params.append('sort', sort);
-  //   }
-  //   if (page) {
-  //     if (page.offset !== undefined) {
-  //       params.append('page[offset]', page.offset);
-  //     }
-  //     if (page.limit !== undefined) {
-  //       params.append('page[limit]', page.limit);
-  //     }
-  //   }
-
-  //   if (params.toString()) {
-  //     url += `?${params.toString()}`;
-  //   }
-  //   return url;
-  // }
-
   async _call(input) {
     try {
-      const url = this.createDrupalURL(input);
+      let url = await this.createDrupalURL(input);
+      // Append apikey to the query with api_key parameter
+      url = new URL(url);
+      url.searchParams.append('api_key', this.apiKey);
+      url = url.toString();
       logger.info(`[DrupalFAAPI] Querying Drupal FA: ${url}`);
       const response = await this.fetchRawText(url);
-      return response;
+      return response.data;
+
     } catch (error) {
       if (error.response && error.response.data) {
         logger.error('[DrupalFAAPI] Error data:', error);
@@ -245,7 +228,6 @@ class DrupalFAAPI extends StructuredTool {
       }
     }
   }
-
 }
 
 module.exports = DrupalFAAPI;
